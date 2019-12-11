52c09bf1a732e62d90d96b7da1f9cc0ba57591f5
airman89 <52982547+ErinMandell@users.noreply.github.com>
2019-12-11 17:52:23 -0600

    finalized weather scrape and working on image scrape

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def index():
 
     # Find one record of data from the mongo database
-    mars = mongo.db.mars.find_one()
+    mars = mongo.db.mars.find()
 
     # Return template and data
     return render_template("index.html", mars=mars)
--- scrape_mars.py
+++ scrape_mars.py
@@ -2,6 +2,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import pandas as pd
 
 
 def init_browser():
@@ -12,10 +13,13 @@ def init_browser():
 
 
 def scrape_info():
+    
+    # **********************************************************
+
     browser = init_browser()
     mars = {}
 
-    # Visit Mars News site
+    # Visit Mars News Site
     url_news = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(url_news)
 
@@ -32,14 +36,10 @@ def scrape_info():
     news_title = result_news.find('div', class_='content_title').text.strip()
     news_p = result_news.find('div', class_='rollover_description_inner').text.strip()
 
-    # create dictionary containing scraped data
-    mars = {
-        "news_title": news_title,
-        "news_paragraph": news_p
-    }
-
     # close news data site
     browser.quit()
+
+    # **********************************************************
 
     # start new browser session for next scrape
     browser = init_browser()
@@ -58,15 +58,78 @@ def scrape_info():
     mars_weather = soup_weather.find('div', class_='js-tweet-text-container').\
     find('p', class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').text.split('pic')[0]
 
+    # close news data site
+    browser.quit()
+
+    # **********************************************************
+
+    # # start new browser session for next scrape
+    # browser = init_browser()
+
+    # # Visit Space Facts site for Mars data
+    # url_facts = "https://space-facts.com/mars/"
+    # browser.visit(url_facts)
+
+    # time.sleep(1)
+
+    # #set variables for scraping facts table
+    # tables = pd.read_html(url_facts)
+
+    # df = tables[0]
+    # df.columns = ['Description','Data']
+
+    # table = df.set_index('Description')
+
+    # mars_table = table.to_html()
+
+    # mars_table = mars_table.replace('\n', '')
+
+    # # create dictionary containing scraped data
+    # mars = {
+    #     "news_title": news_title,
+    #     "news_paragraph": news_p,
+    #     "mars_weather": mars_weather,
+    #     "mars_table": table
+    # }
+
+    # # close data site
+    # browser.quit()
+
+    # **********************************************************
+    
+    # start new browser session for next scrape
+    browser = init_browser()
+
+    # Visit NASA site for image scrape
+    url_full_image = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url_full_image)
+
+    time.sleep(1)
+
+    browser.click_link_by_partial_text('FULL IMAGE')
+
+    # HTML object
+    html_full_image = browser.html
+    
+    # Parse HTML with Beautiful Soup
+    soup_full_image = bs(html_full_image, 'html.parser')
+
+    # Retrieve elements that contain Featured Image information
+    result_full_image = soup_full_image.find('img', class_='fancybox-image')
+
+    # Isolate src and build URL:      
+    mars_image_url = 'https://www.jpl.nasa.gov' + result_full_image['src']    
+
+
     # create dictionary containing scraped data
     mars = {
         "news_title": news_title,
         "news_paragraph": news_p,
-        "mars_weather": mars_weather
+        "mars_weather": mars_weather,
+        "mars_image": mars_image_url
     }
 
-    # close news data site
+    # close image site
     browser.quit()
-
 
     return mars
